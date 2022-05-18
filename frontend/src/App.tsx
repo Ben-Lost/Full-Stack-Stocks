6f466798375de5796497fb1e69d3fe4dec0f2ce8
Benjamin Lostheart <los3@pdx.edu>
2022-05-18 08:02:57 -0700

    site up frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function App() {
+  
+  return (
+    <>
+      <h1> Stock Market Tracker</h1>
+    </>
+  );
+}
+
+
+
+export default App;
